ffdbb2d6e6d12e3f11921004a381ab307bd7cd8b
“ekinast” <24301291+ekinast@users.noreply.github.com>
2024-05-18 15:47:11 -0300

    purchaseOrders

--- back/src/auth/auth.guards.ts
+++ back/src/auth/auth.guards.ts
@@ -1,30 +1,31 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
-function validateAuthorizationHeader(request: Request): boolean {
-  const authHeader = request.headers['authorization'];
-
-  // Verificar si el encabezado Authorization está presente
-  if (!authHeader) {
-    return false;
-  }
-
-  // Verificar si el encabezado Authorization tiene la estructura 'Basic: <email>:<password>'
-  const [method, credentials] = authHeader.split(' ');
-  if (method !== 'Basic' || !credentials) {
-    return false;
-  }
-
-  // Por ahora, no se valida si el email y la contraseña son válidos, solo se verifica su presencia
-  return true;
-}
-
 @Injectable()
 export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    return validateAuthorizationHeader(request);
+    const authHeader = request.headers['authorization'];
+
+    if (!authHeader) {
+      return false;
+    }
+
+    const [method, credentialsBase64] = authHeader.split(' ');
+
+    if (method !== 'Basic' || !credentialsBase64) {
+      return false;
+    }
+
+    const credentials = Buffer.from(credentialsBase64, 'base64').toString(
+      'utf-8',
+    );
+    //console.log('Decoded credentials:', credentials);
+
+    // Aquí continua la lógica de validación de credenciales si es necesario
+
+    return true;
   }
 }
