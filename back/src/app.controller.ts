1e7a6d0afaf9e9eda9327445e56a3f7de3a3de8c
“ekinast” <24301291+ekinast@users.noreply.github.com>
2024-06-26 11:53:22 -0300

    Manejo de favicon

---
+++ back/src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import * as path from 'path';
+import * as fs from 'fs';
+
+@Controller()
+export class AppController {
+  @Get()
+  getRoot(): string {
+    return 'This is a response to GET /';
+  }
+
+  @Get('favicon.ico')
+  getFavicon(@Res() res: Response): void {
+    const faviconPath = path.join(__dirname, '..', 'public', 'favicon.ico');
+    if (fs.existsSync(faviconPath)) {
+      res.sendFile(faviconPath);
+    } else {
+      res.status(404).send('Favicon not found');
+    }
+  }
+}
