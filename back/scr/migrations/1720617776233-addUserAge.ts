e5ceab3709401e9cfa482fbcbfa4040d09954547
“ekinast” <24301291+ekinast@users.noreply.github.com>
2024-07-12 12:52:31 -0300

    AddUserBirthdate

---
+++ back/scr/migrations/1720617776233-addUserAge.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddUserAge1720617776233 implements MigrationInterface {
+    name = 'AddUserAge1720617776233'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users" ADD "birthday" TIMESTAMP NOT NULL DEFAULT now()`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "createdAt"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
+        await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "createdAt"`);
+        await queryRunner.query(`ALTER TABLE "orders" ADD "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "createdAt"`);
+        await queryRunner.query(`ALTER TABLE "orders" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "createdAt"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "birthday"`);
+    }
+
+}
